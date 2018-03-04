9acfa68f1f4f28d10e28c5767b31ce18a7e008d3
skhodparast <sajjad4263@gmail.com>
2018-03-04 23:34:32 +0330

    API Task in order

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,11 @@ var gulp = require('gulp');
 gulp.task('default', function(){
   console.log('Hello World');
 })
+
+gulp.task('one', function(){
+  console.log('task one');
+})
+
+gulp.task('two', ['one'] , function(){
+  console.log('task two');
+})
