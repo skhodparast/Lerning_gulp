d80c5ab30aa929e791664a5a36f1e4344a645e3e
skhodparast <sajjad4263@gmail.com>
2018-03-05 21:32:33 +0330

    do task build -> runSequence

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,46 @@ const dir = {
     build       : '_build/'
   },
 
-gulp          = require('gulp')
+gulp          = require('gulp'),
+runSequence          = require('run-sequence'),
 // gutil         = require('gulp-util'),
 // newer         = require('gulp-newer'),
 // imagemin      = require('gulp-imagemin')
-// sass          = require('gulp-sass'),
+sass          = require('gulp-sass'),
 // postcss       = require('gulp-postcss'),
 // deporder      = require('gulp-deporder'),
-// concat        = require('gulp-concat')
+concat        = require('gulp-concat'),
+concat_css        = require('gulp-concat-css')
 // stripdebug    = require('gulp-strip-debug'),
 // uglify        = require('gulp-uglify')
 ;
 // var browsersync = false;
 
-const copy = {
-  src         : dir.src + 'html/*.html',
-  build       : dir.build + 'html/'
-};
 
 gulp.task('copyFiles', function(){
-	 gulp.src(copy.src)
-		.pipe(gulp.dest(copy.build));
+	 gulp.src(dir.src + 'html/*.html')
+		.pipe(gulp.dest(dir.build + 'html/'));
+});
+
+gulp.task('Sass2css', function(){
+	return gulp
+		.src(dir.src + 'sass/**/*.scss')
+		.pipe(sass({outputstyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest(dir.src + 'css/'));
+});
+
+gulp.task('processCss', function(){
+	gulp.src(dir.src + 'css/*.css')
+		.pipe(concat_css("style.css"))
+		.pipe(gulp.dest(dir.build + 'css/'));
+});
+
+gulp.task('concat_js_files', function(){
+	gulp.src(dir.src + 'js/**/*')
+		.pipe(concat("app.js"))
+		.pipe(gulp.dest(dir.build + 'js/'));
+});
+
+gulp.task('build', function(callback){
+	 runSequence('Sass2css', ['copyFiles', 'processCss', 'concat_js_files'], callback)
 })
