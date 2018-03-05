e2a46ed19957a323710076b1e59b8987102440f0
skhodparast <sajjad4263@gmail.com>
2018-03-05 17:44:22 +0330

    way concat file

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
-var gulp = require('gulp');
+const
 
+dir = {
+    src         : '_src/',
+    build       : '_build/'
+  },
 
-gulp.task('default', ['one', 'two'] , function(){
-  console.log('Hello World');
-})
+gulp          = require('gulp'),
+// gutil         = require('gulp-util'),
+// newer         = require('gulp-newer'),
+// imagemin      = require('gulp-imagemin'),
+// sass          = require('gulp-sass'),
+// postcss       = require('gulp-postcss'),
+// deporder      = require('gulp-deporder'),
+concat        = require('gulp-concat')
+// stripdebug    = require('gulp-strip-debug'),
+// uglify        = require('gulp-uglify')
+;
+// var browsersync = false;
 
-gulp.task('one', function(){
-  console.log('task one');
-})
+const images = {
+  // src         : dir.src + 'js/*',
+  src         : dir.src + 'js/**/*',
+  build       : dir.build + 'js/'
+};
 
-gulp.task('two', ['one'] , function(){
-  console.log('task two');
-})
-
-gulp.task('three', ['one'] , function(){
-  gulp.start('two');
-})
+gulp.task('concat_js_files', function(){
+	gulp.src(images.src)
+		.pipe(concat("app.js"))
+		.pipe(gulp.dest(images.build));
+});
