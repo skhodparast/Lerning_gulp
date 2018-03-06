79984c7287ec1eb44c99c4f04fd22a4f2f77f415
skhodparast <sajjad4263@gmail.com>
2018-03-07 02:06:31 +0330

    use gulp-if & gulp-remove-files & file-exists

--- _src/js/one.js
+++
@@ -1,3 +0,0 @@
-console.log('This ia file: jsone.js')
-
-alert('This alert is in jsone.js')
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,63 @@
-const dir = {
-    src         : '_src/',
-    build       : '_build/'
-  },
+gulp = require('gulp'),
+sass = require('gulp-sass'),
+browserSync = require('browser-sync').create(),
+runSequence = require('run-sequence'),
+removeFiles = require('gulp-remove-files'),
+fileExists = require('file-exists'),
+gulpif = require('gulp-if'),
+concat = require('gulp-concat');
 
-gulp          = require('gulp'),
-// runSequence          = require('run-sequence'),
-// gutil         = require('gulp-util'),
-// newer         = require('gulp-newer'),
-// imagemin      = require('gulp-imagemin')
-// sass          = require('gulp-sass'),
-// postcss       = require('gulp-postcss'),
-// deporder      = require('gulp-deporder'),
-// concat        = require('gulp-concat'),
-// concat_css        = require('gulp-concat-css'),
-// stripdebug    = require('gulp-strip-debug'),
-// uglify        = require('gulp-uglify')
-browserSync        = require('browser-sync').create()
-;
-// var browsersync = false;
-
-gulp.task('watch', ['browserSync'] , function(){
-  gulp.watch(dir.src + 'html/**/*' , browserSync.reload)
+gulp.task('sass2css', function(){
+	return gulp
+		.src('_src/sass/*')
+		.pipe(sass({outputstyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('_dest/css'));
 })
 
 gulp.task('browserSync', function(){
 	browserSync.init({
-    // server: './_src/html',
-    server: dir.src + 'html',
+    server: '',
     port: 8080,
     ui:{ port: 8081}
   })
 });
+
+gulp.task('reload_watch', function(){
+  gulp.start('watch')
+})
+
+// var condition = true;
+var condition = function () {
+  if( fileExists('_dest/css/main.css'))
+    return true;
+};
+
+gulp.task('removeFiles', function(){
+  return gulp.src('_dest/css/main.css')
+  .pipe(gulpif(condition, removeFiles()))
+})
+
+// gulp.task('removeFiles' , function(){
+//   fileExists('_dest/css/main.css').then(exists => {
+//   console.log(exists) // OUTPUTS: true or false
+//   gulp.src('_dest/css/main.css')
+//   .pipe(removeFiles())
+// })
+// });
+
+gulp.task('concat' , function(){
+  return gulp.src('_dest/css/**/*')
+    .pipe(concat("main.css"))
+    .pipe(gulp.dest('_dest/css/'))
+});
+
+gulp.task('build', function(callback){
+	 runSequence('removeFiles', 'sass2css', ['concat'], callback)
+})
+
+gulp.task('watch' , function(){
+  gulp.watch('_src/sass/**/*' , ['build'])
+  // gulp.watch('_src/sass/**/*' , ['concat'])
+  gulp.watch('index.html' , browserSync.reload)
+});
+gulp.task('default', ['browserSync', 'watch'] ,function(){})
